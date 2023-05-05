48652d314ba49ee08cf6cd1e57a0c5089a641e71
namtv <81201592+ThanNamSOS@users.noreply.github.com>
2023-05-05 23:11:30 +0700

    Update SendmailApplication.java

--- src/main/java/com/example/sendmail/SendmailApplication.java
+++ src/main/java/com/example/sendmail/SendmailApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SendmailApplication {
 
     public static void main(String[] args) {
+         System.out.println("hello namtv");
         SpringApplication.run(SendmailApplication.class, args);
     }
 
